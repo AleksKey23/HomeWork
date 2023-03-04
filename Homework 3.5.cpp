a3de3948126978f6f92771da2f301eeec9143059
AleksKey23 <123273754+AleksKey23@users.noreply.github.com>
2023-03-04 16:26:13 +0300

    Add files via upload

---
+++ Homework 3.5.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <iostream>
+
+/*ссылка на репозиторий - https://github.com/AleksKey23/HomeWork.git */
+
+main()
+{	
+  int a=1;
+  while (a<10)
+  { printf ("Hello\n");
+    a=++a; }
+    
+    printf ("\n");
+  
+  int b=0;
+  while (b<15)
+   { printf (" %i ", b);
+    b=b+1; }
+    
+    printf ("\n Nechetnie \n");
+  int c=1;
+  while (c<15)
+   { printf ("  %i ", c);
+    c=c+2; }
+    
+    printf ("\n\n");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  int i=5;
+  int sum=0;
+  while (i<15)
+  { sum=sum+i;
+    printf ("%i+", i);
+    i=i+1; }
+  sum=sum+i;
+	printf ("%i = %i ", i, sum);
+    
+    
+    
+    
+    
+    
+    
+    
+}
