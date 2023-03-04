2590759d743c60e064130ddcf77ed084dc21ed96
AleksKey23 <123273754+AleksKey23@users.noreply.github.com>
2023-03-05 00:34:53 +0300

    Add files via upload

---
+++ Homework 3.7.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+
+/*ссылка на репозиторий - https://github.com/AleksKey23/HomeWork.git */
+
+main()
+{	
+ int x;
+    int sum=0;
+    
+    for (x=1;x<100;x=x+1)
+    {
+        sum=sum+x;
+        printf ("%i+", x);
+    }
+    
+     sum=sum+x;
+    printf ("%i = %i", x, sum);
+ 
+ 
+}
